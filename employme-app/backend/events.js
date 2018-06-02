ef822611ea4b5921dc89fd74952747e11428fe52
Cason Gordy <casongordy@gmail.com>
2018-06-02 16:59:18 -0400

    wip: 1.1

---
+++ employme-app/backend/events.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+      id: 1,
+      title: 'Cut Grass',
+      detail: 'In Marietta',
+      date: '2018-06-21'
+    },
+    {
+      id: 2,
+      title: 'Paint House',
+      detail: 'Need done this weekend',
+      date: '2017-06-21'
+    },
+    {
+      id: 3,
+      title: 'Walk Dog',
+      detail: 'Downtown Atlanta'
+      date: '2017-06-13'
+    }
+  ];
